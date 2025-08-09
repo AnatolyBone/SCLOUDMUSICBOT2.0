c3538b4dbb4951dce63281a7ba79451208ea8352
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-08-09 23:30:02 +0300

    Delete modules directory

--- modules/utils/telegramUtils.js
+++
@@ -1,9 +0,0 @@
-export async function safeTelegramCall(ctx, method, ...args) {
-  try {
-    return await ctx.telegram[method](...args);
-  } catch (e) {
-    console.error(`Ошибка вызова Telegram API метода ${method}:`, e);
-    // Можно добавить логирование в БД или отправку уведомления админу
-    return null;
-  }
-}
