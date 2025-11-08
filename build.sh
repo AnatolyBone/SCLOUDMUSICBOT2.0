06341b8d90a76c4eb57642268ff7f7d3bfcc9dd0
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-11-08 06:56:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,15 @@
 set -o errexit
 
 # 1. Устанавливаем Node.js зависимости
+echo "---> Installing Node.js dependencies..."
 npm install
 
-# 2. Устанавливаем Python зависимости
+# 2. Устанавливаем Python зависимости из файла
+echo "---> Installing Python dependencies..."
 pip install -r requirements.txt
+
+# 3. Устанавливаем yt-dlp как системную утилиту
+echo "---> Installing yt-dlp globally..."
+pip install yt-dlp --no-deps -U -t /usr/local/bin
+
+echo "Build script finished."
