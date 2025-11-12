1563f67f5d0074e6eb417e53cdc0a0759de3893e
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-11-12 07:05:46 +0300

    Update downloadManager.js

--- services/downloadManager.js
+++ services/downloadManager.js
@@ -18,8 +18,6 @@ import os from 'os';
 import { fileURLToPath } from 'url';
 import crypto from 'crypto';
 import ytdl from 'youtube-dl-exec';
-import ytsr from 'ytsr'; // npm install ytsr
-import ytdlCore from 'ytdl-core';
 import { bot } from '../bot.js';
 import { T } from '../config/texts.js';
 import { TaskQueue } from '../lib/TaskQueue.js';
