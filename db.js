0b67a88b4863f0c05a05345f98fbcf50452779af
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2026-05-21 12:05:04 +0300

    Update db.js

--- db.js
+++ db.js
@@ -4,8 +4,15 @@ import { Pool } from 'pg';
 import { createClient } from '@supabase/supabase-js';
 import ws from 'ws';
 import { SUPABASE_URL, SUPABASE_KEY, DATABASE_URL } from './config.js';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY,
+  {
+    realtime: {
+      transport: ws
+    }
+  }
+);
 export const pool = new Pool({
   connectionString: DATABASE_URL,
   ssl: { rejectUnauthorized: false },
